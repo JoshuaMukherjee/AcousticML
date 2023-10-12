c961bee13ccad833b617edaf1930bcb39a5c3b6a
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2023-10-12 09:43:01 +0100

    Results FCNN

---
+++ Results/ResultsFCNN.py
@@ -0,0 +1,96 @@
+import matplotlib.pyplot as plt
+import os,sys
+import torch
+import pickle
+import numpy as np
+from torch.utils.data import DataLoader 
+
+
+p = os.path.abspath('.')
+sys.path.insert(1, p)
+
+from Solvers import wgs, gspat
+from Dataset import PointDataset, FDataset
+from Utilities import propagate, forward_model, device
+
+
+if "-l" in sys.argv:
+    model_name = sys.argv[1]
+
+    model = torch.load("Models/model_"+model_name+".pth")
+
+    loss = pickle.load(open("Losses/loss_"+model_name+".pth","rb"))
+    train,test = loss
+    print(len(train))
+    plt.plot(train,label="train")
+    plt.plot(test,label="test")
+    plt.yscale("symlog")
+    
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+
+    try:
+        max_epoch = model.epoch_saved
+        plt.plot(max_epoch,test[max_epoch],"x",label="Best Test Loss")
+    except AttributeError:
+        pass
+
+    plt.legend()
+    plt.show()
+
+if "-p" in sys.argv:
+    
+    model_name = sys.argv[1]
+
+    model = torch.load("Models/model_"+model_name+".pth")
+
+    N = 4
+    P = 5
+    dataset = FDataset(P,N)
+    data = iter(DataLoader(dataset,1,shuffle=True))
+
+    
+    press = []
+    mins =[]
+    wgs_200_ps = []
+    gs_pat_ps = []
+
+
+    for F,p,a,pr in data:
+
+
+        out = model(F)
+        
+        pressure = torch.abs(propagate(out,p))
+
+        wgs_p = torch.abs(pr)
+      
+        A = forward_model(p[0,:])
+        backward = torch.conj(A).T
+        R = A@backward
+        _,pres = gspat(R,A,backward,torch.ones(N,1).to(device)+0j, 200)
+
+        gs_pat_p = torch.abs(pres)
+
+        press.append(pressure.cpu().detach().numpy())
+        wgs_200_ps.append(wgs_p.squeeze_().cpu().detach().numpy())
+        gs_pat_ps.append(gs_pat_p.squeeze_().cpu().detach().numpy())
+
+    fig, axs = plt.subplots(1,P)
+    fig.tight_layout()
+    for i in range(P):
+        to_plot = {}
+        
+        to_plot["Model"] = press[i]
+        to_plot["WGS"] = wgs_200_ps[i]
+        to_plot["GS PAT"] = gs_pat_ps[i]
+
+        axs[i].boxplot(to_plot.values())
+        axs[i].set_xticklabels(to_plot.keys(), rotation=90)
+        axs[i].set_ylim(bottom=0,top=13000)
+        axs[i].set_yticklabels(range(0,13000,2000), rotation=90)
+        axs[i].set_ylabel("Pressure (Pa)")
+
+        print(press[i])
+        
+    plt.show()
