d29b6db217e86c677325bb1be594519385db0bad
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2023-10-27 15:54:57 +0100

    Constants

--- Constants.py
+++ Constants.py
@@ -1,8 +1,9 @@
 
 pi = 3.1415926535
-V = 4/3 * pi * .001**3
+R = .001
+V = 4/3 * pi * R**3
 c_0 = 343
 p_0 = 1.2
 c_p = 1052
-p_p = 1150
+p_p = 29.36 #From Holographic acoustic elements for manipulation of levitated objects
 f = 40000
