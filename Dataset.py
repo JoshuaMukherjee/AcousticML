2bd6796bc8d2b16ebcdeb722d894b6faa0fa88d7
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2023-11-13 10:24:05 +0000

    Pressure Target Dataset

--- Dataset.py
+++ Dataset.py
@@ -163,13 +163,59 @@ class NaiveDataset(Dataset):
          '''
          return self.points[i],self.activations[i],self.pressures[i], self.naive_acts[i]
         
+class PressureTargetDataset(Dataset):
+    '''
+    outputs points, activations, pressures, naive_act
+    '''
+    def __init__(self,length,N=4,seed=None):
+        self.length = length #Number of point sets in the Dataset (length)
+        self.N = N #Number of points per set
+        self.seed = seed #custom seed
+    
+        self.points = []
+        self.activations = []
+        self.pressures = []
+        self.naive_acts = []
+        self.targets = []
+        print(self.length)
+
+        for batch in range(self.length):
+            points = torch.FloatTensor(3,self.N).uniform_(-.06,.06).to(device)
+            naive_p, naive_act = naive_solver(points)
+            A=forward_model(points, transducers()).to(device)
+            _, _, x = wgs(A,torch.ones(self.N,1).to(device)+0j,200)
+            pressures = A@x[:,0]
+            activations = x[:,0]
+
+            targets = torch.FloatTensor(self.N).uniform_(6000,10000).to(device)
+
+
+            self.points.append(points)
+            self.pressures.append(pressures)
+            self.activations.append(activations)
+            self.naive_acts.append(naive_act)
+            self.targets.append(targets)
+
+            if batch % 200 == 0:
+                print(batch,end=" ",flush=True)
+    
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self,i):
+         '''
+         returns points, activations, pressures
+         '''
+         return self.points[i],self.activations[i],self.pressures[i], self.naive_acts[i], self.targets[i]
+
+
 
 if __name__ == "__main__":
     
-    length = 600000
-    test_length = 1000 
-    N = 10
-    dataset_type = NaiveDataset
+    length = 4
+    test_length = 2 
+    N = 4
+    dataset_type = PressureTargetDataset
     
 
     
