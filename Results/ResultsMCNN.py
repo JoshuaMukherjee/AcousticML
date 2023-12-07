3d82e80181f51e2649d30e5b3e09b39d7bfc2108
JoshuaMukherjee <joshuaakmukherjee@gmail.com>
2023-12-07 16:26:00 +0000

    gorkov results scatter

--- Results/ResultsMCNN.py
+++ Results/ResultsMCNN.py
@@ -10,10 +10,13 @@ p = os.path.abspath('.')
 sys.path.insert(1, p)
 
 from acoustools.Solvers import wgs, gspat,naive_solver, naive_solver_batch
-from Dataset import PointDataset, FDataset, FDatasetNorm, NaiveDataset,PressureTargetDataset
-from acoustools.Utilities import propagate, forward_model, device, do_NCNN, create_points, add_lev_sig, propagate_abs, create_board, generate_pressure_targets
+from Dataset import PointDataset, FDataset, FDatasetNorm, NaiveDataset,PressureTargetDataset, GorkovTargetDataset
+from acoustools.Utilities import propagate, forward_model, device, create_points, add_lev_sig, propagate_abs, create_board, generate_pressure_targets
 from acoustools.Visualiser import Visualise, Visualise_single, get_point_pos
-from acoustools.Gorkov import gorkov_autograd
+from acoustools.Gorkov import gorkov_autograd, gorkov_analytical
+
+
+from MLUtilities import do_NCNN
 
 if "-latest" in sys.argv:
     latest = "_latest"
@@ -119,6 +122,66 @@ if "-sp" in sys.argv:
     p = np.poly1d(z)
 
     x_lin = np.linspace(4000,11000)
+    plt.plot(xs, p(xs), "r-",label="Trend Line, m="+str(round(z[0],2))+", c="+str(round(z[1],0)))
+    plt.plot(xs, xs, "b-", label="target=output")
+    plt.legend()
+    plt.show()
+
+if "-sg" in sys.argv:
+
+    model_name = sys.argv[1]
+
+    model = torch.load("Models/model_"+model_name+latest+".pth", map_location=torch.device(device))
+    total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(total_params)
+
+    P = 100
+    N = 4
+    if "-overfit" not in sys.argv:
+        dataset = GorkovTargetDataset(P,N)
+        print("Generated data...")
+        
+    else:
+        print("Overfit")
+        dataset = torch.load("./Datasets/GorkovTargetDataset"+norm+"Train-4-4.pth")
+        P = len(dataset)
+
+    data = iter(DataLoader(dataset,1,shuffle=True))
+    xs = []
+    ys = []
+    ins = []
+    for p,a,targets in data:
+        targets = targets.squeeze_()
+        
+
+        act_in = torch.reshape(a,(1,2,16,16))
+        act_phases = torch.angle(act_in)
+        
+        activation_out_img = model(act_phases, targets) 
+        
+        activation_out = torch.reshape(activation_out_img,(1,512,1))
+        # activation = add_lev_sig(activation_out)
+        U = gorkov_analytical(activation_out, p)
+
+        input_U = gorkov_analytical(a, p)
+
+        ys = ys+[y.detach().cpu().item() for y in torch.squeeze(U)]
+        xs = xs+[x.detach().cpu().item() for x in torch.squeeze(targets)]
+        ins = ins+[i.detach().cpu().item() for i in torch.squeeze(input_U)]
+       
+    
+    plt.scatter(xs,ys, label="Network")
+    plt.scatter(xs,ins, label="Gradient Solver")
+    plt.xlim(-1e-4,0)
+    plt.ylim(-1e-4,0)
+
+    plt.xlabel("Target (Pa)")
+    plt.ylabel("Output (Pa)")
+    z = np.polyfit(xs,ys,1)
+    print(z)
+    p = np.poly1d(z)
+
+    x_lin = np.linspace(-1e-4,0)
     plt.plot(xs, p(xs), "r-",label="Trend Line, m="+str(round(z[0],2))+", c="+str(round(z[1],0)))
     plt.plot(xs, xs, "b-", label="target=output")
     plt.legend()
